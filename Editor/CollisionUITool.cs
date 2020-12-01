838815dac6ed7e0c681723d9d10d75c1587a6385
ilkp <ilkka.pokkinen@gmail.com>
2020-12-01 23:39:11 +0200

    Color picker for Visuals

--- Editor/CollisionUITool.cs
+++ Editor/CollisionUITool.cs
@@ -16,17 +16,19 @@ public class CollisionUITool : EditorWindow
 		public SerializedObject SerializedColDetector { get; set; }
 	}
 
-	private const int SPACE = 20;
-	private const int SPACE_MARGIN = 10;
-	private const int ACTIVE_WIDTH = 60;
-	private const int NAME_WIDTH = 200;
-	private const int TOGGLE_WIDTH = 90;
-	private const int TOGGLE_ALL_WIDTH = 40;
+	private const float SPACE = 20;
+	private const float SPACE_MARGIN = 10;
+	private const float NAME_WIDTH = 200;
+	private const float TOGGLE_WIDTH = 75;
+	private const float TOGGLE_ALL_WIDTH = 30;
+	private const float COLOR_WIDTH = 75;
+	private const float SERIALIZED_PROPERTY_MIN_WIDTH = 260;
 	private const float DOUBLE_CLICK_TIME = 0.2f;
 
 	private Vector2 horizontalScollView;
 	private Vector2 verticalScrollView;
 	private bool dummyToggle = false;
+	private Color dummyColor = Color.gray;
 
 	private int selectedRootObject = 0;
 	private string[] rootObjectNames;
@@ -36,15 +38,15 @@ public class CollisionUITool : EditorWindow
 	private bool executeFocus = false;
 	private bool focusing = false;
 
-	private readonly GUILayoutOption[] activeOptions = new GUILayoutOption[] { GUILayout.Width(ACTIVE_WIDTH) };
 	private readonly GUILayoutOption[] toggleOptions = new GUILayoutOption[] { GUILayout.Width(TOGGLE_WIDTH) };
 	private readonly GUILayoutOption[] nameOptions = new GUILayoutOption[] { GUILayout.Width(NAME_WIDTH) };
 	private readonly GUILayoutOption[] toggleAllOptions = new GUILayoutOption[] { GUILayout.Width(TOGGLE_ALL_WIDTH) };
-	private readonly GUILayoutOption[] ignoredColOptions = new GUILayoutOption[] { GUILayout.MinWidth(TOGGLE_WIDTH * 2), GUILayout.MaxWidth(TOGGLE_WIDTH * 4) };
+	private readonly GUILayoutOption[] colorOptions = new GUILayoutOption[] { GUILayout.Width(COLOR_WIDTH * 2f) };
+	private readonly GUILayoutOption[] ignoredColOptions = new GUILayoutOption[] { GUILayout.MinWidth(SERIALIZED_PROPERTY_MIN_WIDTH), GUILayout.ExpandWidth(true) };
 
 	private Texture2D[] rowTex;
-	private GUIStyle headerStyle;
-	private GUIStyle headerStyleIgnoredCol;
+	private GUIStyle headerStyleCentered;
+	private GUIStyle headerStyleLeft;
 	private GUIStyle rowStyle;
 
 
@@ -67,8 +69,8 @@ public class CollisionUITool : EditorWindow
 	{
 		CreateToggleContent();
 		InitBackgroundTex();
-		headerStyle = new GUIStyle(EditorStyles.label) { alignment = TextAnchor.MiddleCenter };
-		headerStyleIgnoredCol = new GUIStyle(EditorStyles.label) { alignment = TextAnchor.MiddleLeft };
+		headerStyleCentered = new GUIStyle(EditorStyles.label) { alignment = TextAnchor.MiddleCenter };
+		headerStyleLeft = new GUIStyle(EditorStyles.label) { alignment = TextAnchor.MiddleLeft };
 		rowStyle = new GUIStyle();
 		rowStyle.normal.background = rowTex[0];
 	}
@@ -133,6 +135,7 @@ public class CollisionUITool : EditorWindow
 		}
 		EditorGUILayout.EndScrollView();
 		EditorGUILayout.EndScrollView();
+		GUILayout.Space(SPACE_MARGIN);
 
 		EditorGUILayout.EndHorizontal();
 		EditorGUILayout.EndVertical();
@@ -175,37 +178,24 @@ public class CollisionUITool : EditorWindow
 	private void CreateHeader()
 	{
 		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.LabelField("Active", headerStyle, activeOptions);
-		EditorGUILayout.LabelField("Name", headerStyle, nameOptions);
-		EditorGUILayout.LabelField("Collisions", headerStyle, toggleOptions);
-		EditorGUILayout.LabelField("Sounds", headerStyle, toggleOptions);
-		EditorGUILayout.LabelField("Visualization", headerStyle, toggleOptions);
-		GUILayout.Space(SPACE);
-		EditorGUILayout.LabelField("Ignored Colliders", headerStyleIgnoredCol, ignoredColOptions);
+		EditorGUILayout.LabelField("Active", headerStyleCentered, toggleOptions);
+		EditorGUILayout.LabelField("Name", headerStyleCentered, nameOptions);
+		EditorGUILayout.LabelField("Collisions", headerStyleCentered, toggleOptions);
+		EditorGUILayout.LabelField("Sounds", headerStyleCentered, toggleOptions);
+		EditorGUILayout.LabelField("Visualization", headerStyleCentered, toggleOptions);
+		EditorGUILayout.LabelField("Color", headerStyleCentered, colorOptions);
+		EditorGUILayout.LabelField("Ignored Colliders", headerStyleLeft, ignoredColOptions);
 		EditorGUILayout.EndHorizontal();
 	}
 
 	private void CreateToggleAllButtons()
 	{
 		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.LabelField("", activeOptions);
+		EditorGUILayout.LabelField("", toggleOptions);
 		EditorGUILayout.LabelField("", nameOptions);
-
-		EditorGUILayout.BeginHorizontal(toggleOptions);
-		if (GUILayout.Button("all", toggleAllOptions)) ToggleAll<CollisionDetector>(true);
-		if (GUILayout.Button("none", toggleAllOptions)) ToggleAll<CollisionDetector>(false);
-		EditorGUILayout.EndHorizontal();
-
-		EditorGUILayout.BeginHorizontal(toggleOptions);
-		if (GUILayout.Button("all", toggleAllOptions)) ToggleAll<CollisionSoundManager>(true);
-		if (GUILayout.Button("none", toggleAllOptions)) ToggleAll<CollisionSoundManager>(false);
-		EditorGUILayout.EndHorizontal();
-
-		EditorGUILayout.BeginHorizontal(toggleOptions);
-		if (GUILayout.Button("all", toggleAllOptions)) ToggleAll<Visuals>(true);
-		if (GUILayout.Button("none", toggleAllOptions)) ToggleAll<Visuals>(false);
-		EditorGUILayout.EndHorizontal();
-
+		CreateToggleAllButton<CollisionDetector>();
+		CreateToggleAllButton<CollisionSoundManager>();
+		CreateToggleAllButton<Visuals>();
 		EditorGUILayout.EndHorizontal();
 	}
 
@@ -215,7 +205,7 @@ public class CollisionUITool : EditorWindow
 		EditorGUILayout.BeginHorizontal(rowStyle);
 
 		EditorGUI.BeginChangeCheck();
-		goId.Active = NoLabelToggle(goId.Active, activeOptions);
+		goId.Active = NoLabelToggle(goId.Active, toggleOptions);
 		if (EditorGUI.EndChangeCheck())
 			go.SetActive(goId.Active);
 
@@ -226,17 +216,19 @@ public class CollisionUITool : EditorWindow
 				executeFocus = true;
 			clickTime = EditorApplication.timeSinceStartup;
 		}
-		CreateToggle<CollisionDetector>(go);
-		CreateToggle<CollisionSoundManager>(go);
-		CreateToggle<Visuals>(go);
+
+		CreateToggle(go.GetComponent<CollisionDetector>());
+		CreateToggle(go.GetComponent<CollisionSoundManager>());
+		CreateToggle(go.GetComponent<Visuals>());
+		CreateColorPicker(go.GetComponent<Visuals>());
 		CreateColliderIgnoreArr(goId);
 		EditorGUILayout.EndHorizontal();
 	}
 
-	private void CreateToggle<T>(GameObject go) where T : Behaviour
+	private void CreateToggle<T>(T component) where T : Behaviour
 	{
-		if (go.GetComponent<T>())
-			go.GetComponent<T>().enabled = NoLabelToggle(go.GetComponent<T>().enabled, toggleOptions);
+		if (component)
+			component.enabled = NoLabelToggle(component.enabled, toggleOptions);
 		else
 		{
 			GUI.enabled = false;
@@ -244,10 +236,34 @@ public class CollisionUITool : EditorWindow
 			GUI.enabled = true;
 		}
 	}
+	private void CreateToggleAllButton<T>() where T : Behaviour
+	{
+		EditorGUILayout.BeginHorizontal(toggleOptions);
+		GUILayout.FlexibleSpace();
+		if (GUILayout.Button("on", toggleAllOptions)) ToggleAll<T>(true);
+		if (GUILayout.Button("off", toggleAllOptions)) ToggleAll<T>(false);
+		GUILayout.FlexibleSpace();
+		EditorGUILayout.EndHorizontal();
+	}
+
+	private void CreateColorPicker(Visuals visuals)
+	{
+		EditorGUILayout.BeginHorizontal(colorOptions);
+		GUILayout.FlexibleSpace();
+		if (visuals != null)
+			visuals.color = EditorGUILayout.ColorField(visuals.color, GUILayout.Width(COLOR_WIDTH));
+		else
+		{
+			GUI.enabled = false;
+			dummyColor = EditorGUILayout.ColorField(dummyColor, GUILayout.Width(COLOR_WIDTH));
+			GUI.enabled = true;
+		}
+		GUILayout.FlexibleSpace();
+		EditorGUILayout.EndHorizontal();
+	}
 
 	private void CreateColliderIgnoreArr(GoId goId)
 	{
-		GUILayout.Space(SPACE);
 		SerializedProperty ignoredColliders = goId.SerializedColDetector.FindProperty("ignoredColliders");
 		EditorGUILayout.PropertyField(ignoredColliders, new GUIContent { text = "Size: " + ignoredColliders.arraySize }, true, ignoredColOptions);
 		goId.SerializedColDetector.ApplyModifiedProperties();
@@ -257,11 +273,11 @@ public class CollisionUITool : EditorWindow
 	{
 		float originalLabelWidth = EditorGUIUtility.labelWidth;
 		EditorGUIUtility.labelWidth = 0.1f;
-		GUILayout.BeginHorizontal(horizontalOptions);
+		EditorGUILayout.BeginHorizontal(horizontalOptions);
 		GUILayout.FlexibleSpace();
 		value = EditorGUILayout.Toggle(value);
 		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
+		EditorGUILayout.EndHorizontal();
 		EditorGUIUtility.labelWidth = originalLabelWidth;
 		return value;
 	}
